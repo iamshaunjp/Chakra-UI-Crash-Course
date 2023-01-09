8749e450ba2fb4e95b5c580091505e3c4d77b18d
Shaun <shaun@thenetninja.co.uk>
2023-01-09 16:46:09 +0000

    lesson-3

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,12 @@ import { Flex, Heading, Box, Text, Button, Spacer, HStack } from "@chakra-ui/rea
 
 export default function Navbar() {
   return (
-    <Flex as="nav" p="10px" alignItems="center">
-      <Heading as="h1">Dojo Tasks</Heading>
+    <Flex as="nav" p="10px" mb="60px" alignItems="center">
+      <Heading as="h1" fontSize="1.5em">Dojo Tasks</Heading>
       <Spacer />
 
       <HStack spacing="20px"> 
-        <Box bg="gray.200" p="10px">M</Box>
+        <Box bg="gray.200" p="10px 15px" borderRadius="50%">M</Box>
         <Text>mario@netninja.dev</Text>
         <Button colorScheme="purple">Logout</Button>
       </HStack>
