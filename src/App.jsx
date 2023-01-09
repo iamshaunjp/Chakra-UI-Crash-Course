8bc96961d85c7d2cc47a06464d22492940fb3a04
Shaun <shaun@thenetninja.co.uk>
2023-01-09 15:01:49 +0000

    starter-project

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { 
+  createBrowserRouter, 
+  createRoutesFromElements, 
+  Route, 
+  RouterProvider 
+} from 'react-router-dom'
+
+// layouts and pages
+import RootLayout from './layouts/RootLayout'
+import Dashboard from './pages/Dashboard'
+import Create from './pages/Create'
+import Profile from './pages/Profile'
+
+// router and routes
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Dashboard />} />
+      <Route path="create" element={<Create />} />
+      <Route path="profile" element={<Profile />} />
+    </Route>
+  )
+)
+
+function App() {
+  return (
+    <RouterProvider router={router} />
+  )
+}
+
+export default App
---
+++ src/pages/Create.jsx
@@ -0,0 +1,5 @@
+export default function Create() {
+  return (
+    <div>Create</div>
+  )
+}
