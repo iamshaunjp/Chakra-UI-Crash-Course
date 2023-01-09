f1cac3435857c1d47daec8787aafd97c60382e24
Shaun <shaun@thenetninja.co.uk>
2023-01-09 15:35:43 +0000

    lesson-1

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
+import { ChakraProvider } from '@chakra-ui/react'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ChakraProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ChakraProvider>
 )
