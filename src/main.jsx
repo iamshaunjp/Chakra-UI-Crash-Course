8731da78c5d09f18a70442153f2ec42fecd5c3b8
Shaun <shaun@thenetninja.co.uk>
2023-01-10 18:11:38 +0000

    lesson-12

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+
+// extending the theme
+const colors = {
+  brand: {
+    900: '#024fc9',
+    800: '#146af5',
+    700: '#2977f2',
+    600: '#337df2',
+    500: '#4287f5'
+  },
+}
+const fonts = {
+  body: 'Tahoma',
+  heading: 'Courier New'
+}
+
+const theme = extendTheme({ colors, fonts })
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <ChakraProvider>
+  <ChakraProvider theme={theme}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
