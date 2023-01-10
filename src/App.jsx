a77fd8e6205d0bdbdc8d49c8a742342615848b23
Shaun <shaun@thenetninja.co.uk>
2023-01-10 17:07:28 +0000

    lesson-7

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import {
 
 // layouts and pages
 import RootLayout from './layouts/RootLayout'
-import Dashboard from './pages/Dashboard'
+import Dashboard, { tasksLoader } from './pages/Dashboard'
 import Create from './pages/Create'
 import Profile from './pages/Profile'
 
@@ -15,7 +15,7 @@ import Profile from './pages/Profile'
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
-      <Route index element={<Dashboard />} />
+      <Route index element={<Dashboard />} loader={tasksLoader} />
       <Route path="create" element={<Create />} />
       <Route path="profile" element={<Profile />} />
     </Route>
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,23 +1,61 @@
-import { Box, SimpleGrid } from "@chakra-ui/react";
+import { EditIcon, ViewIcon } from "@chakra-ui/icons"
+import { 
+  Box, 
+  SimpleGrid,
+  Text,
+  Flex,
+  Heading,
+  Card, 
+  CardHeader,
+  CardBody,
+  CardFooter,
+  HStack,
+  Divider,
+  Button
+} from "@chakra-ui/react"
+import { useLoaderData } from "react-router-dom"
 
 export default function Dashboard() {
+  const tasks = useLoaderData()
 
   return (
-    <SimpleGrid p="10px" columns={4} spacing={10} minChildWidth={250}>
-      <Box bg="white" height="200px" border="1px solid"></Box>
-      <Box bg="white" height="200px" border="1px solid"></Box>
-      <Box bg="white" height="200px" border="1px solid"></Box>
-      <Box bg="white" height="200px" border="1px solid"></Box>
-      
-      <Box bg="white" height="200px" border="1px solid"></Box>
-      <Box bg="white" height="200px" border="1px solid"></Box>
-      <Box bg="white" height="200px" border="1px solid"></Box>
-      <Box bg="white" height="200px" border="1px solid"></Box>
-      
-      <Box bg="white" height="200px" border="1px solid"></Box>
-      <Box bg="white" height="200px" border="1px solid"></Box>
-      <Box bg="white" height="200px" border="1px solid"></Box>
-      <Box bg="white" height="200px" border="1px solid"></Box>
+    <SimpleGrid spacing={10} minChildWidth={300}>
+      {tasks && tasks.map(task => (
+        <Card key={task.id} borderTop="8px" borderColor="purple.400" bg="white">
+
+          <CardHeader color="gray.700">
+            <Flex gap={5}>
+              <Box w="50px" h="50px">
+                <Text>AV</Text>
+              </Box>
+              <Box>
+                <Heading as="h3" size="sm">{task.title}</Heading>
+                <Text>by {task.author}</Text>
+              </Box>
+            </Flex>
+          </CardHeader>
+
+          <CardBody color="gray.500">
+            <Text>{task.description}</Text>
+          </CardBody>
+
+          <Divider borderColor="gray.200" />
+
+          <CardFooter>
+            <HStack>
+              <Button variant="ghost" leftIcon={<ViewIcon />}>Watch</Button>
+              <Button variant="ghost" leftIcon={<EditIcon />}>Comment</Button>
+            </HStack>
+          </CardFooter>
+
+        </Card>
+      ))}
     </SimpleGrid>
   )
+}
+
+export const tasksLoader = async () => {
+  const res = await fetch('http://localhost:3000/tasks')
+
+  return res.json()
 }
