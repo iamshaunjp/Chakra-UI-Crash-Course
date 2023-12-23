e0a4f8fd499c14a686f209880695acf8dcb4048e
shubhamyadav97660@gmail.com <shubhamyadav97660@gmail.com>
2023-12-23 22:36:38 +0530

    second commit

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,37 @@
-import { Heading } from "@chakra-ui/react";
+import { Container, Heading, Text, Box } from "@chakra-ui/react";
 
 export default function Dashboard() {
+
+  const boxStyles = {
+    p: "20px",
+    bg: "purple.400",
+    c: "white",
+    m: "10px",
+    textAlign: "center"
+  }
+
   return (
-    <Heading> This is Dashboard </Heading>
-  )
+    <Container as="section" maxWidth="4xl" py="20px">
+      <Heading my="30px" p="30px"> This is Dashboard </Heading>
+      <Text marginLeft="30px">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam,
+        doloribus.
+      </Text>
+      <Text ml="30px" color="blue.300" fontWeight="bold">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo quod dolorem maxime quos ut officia.
+      </Text>
+
+      <Box my="30px" bg="orange" p="20px">
+        <Text color="white">
+          This is a box
+        </Text>
+      </Box>
+
+      <Box sx={boxStyles}>
+        <Text>
+          This is the editable style
+        </Text>
+      </Box>
+    </Container>
+  );
 }
