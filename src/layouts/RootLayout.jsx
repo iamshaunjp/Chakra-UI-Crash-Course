cf14f7cffe11f30f223a178d92f17e2b7545a20d
shubhamyadav97660@gmail.com <shubhamyadav97660@gmail.com>
2023-12-28 21:23:19 +0530

    Completed course

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -10,7 +10,7 @@ export default function RootLayout() {
       <GridItem
         as="aside"
         colSpan={{ base: 6, lg: 2, xl: 1}}
-        bg="purple.400"
+        bg="brand.500"
         minHeight={{lg: "100vh"}}
         p={{base: '20px', lg: '30px'}}
       >
