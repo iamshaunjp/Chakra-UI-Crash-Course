158851da5099d381c47f434293a716b9a831d35d
shubhamyadav97660@gmail.com <shubhamyadav97660@gmail.com>
2023-12-28 20:12:56 +0530

    eleventh commit

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,15 +1,26 @@
-import { Tab, TabList, TabPanels, Tabs } from "@chakra-ui/react";
+import { ListIcon, Tab, TabList, TabPanel, TabPanels, Tabs, List, ListItem } from "@chakra-ui/react";
+import { ChatIcon, EmailIcon } from "@chakra-ui/icons";
 
 export default function Profile() {
   return (
-    <Tabs mt="40px" p="20px" colorScheme="purple">
+    <Tabs mt="40px" p="20px" colorScheme="purple" variant={"enclosed"}>
       <TabList>
-        <Tab>Account Info</Tab>
-        <Tab>Task History</Tab>
+        <Tab _selected={{ color: 'white', bg: 'purple.400' }}>Account Info</Tab>
+        <Tab _selected={{ color: 'white', bg: 'purple.400' }}>Task History</Tab>
       </TabList>
 
       <TabPanels>
-
+        <TabPanel>
+          <List>
+            <ListItem>
+              <ListIcon as={EmailIcon}/>
+              Email: mario@netninja.com
+            </ListItem>
+            <ListItem>
+              <ListIcon as={ChatIcon}/>
+            </ListItem>
+          </List>
+        </TabPanel>
       </TabPanels>
     </Tabs>
   )
